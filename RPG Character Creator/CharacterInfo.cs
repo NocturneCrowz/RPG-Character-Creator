45508467cd40cd7a8d9eccea9c67c2c03dfcff1c
Cristian <531tonk@gmail.com>
2020-04-28 15:37:25 +0200

    Roller class, Character info class
    Added the DiceRoller class and the CharacterInfo class, with a small main for testing

---
+++ RPG Character Creator/CharacterInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_Character_Creator
+{
+    class CharacterInfo
+    {
+        // Character's basic information
+        private string _dateOfBirth;
+        private string _placeOfBirth;
+        private string _name;
+        private string _sex;
+        private int _age;
+        private int _height;
+        private int _weight;
+        private string _size;
+        private string _hair;
+        private string _eyes;
+        private string _skinTone;
+
+        // Those functions will let me acquire and write the proper data of the character
+        public string DateOfBirth { get => this._dateOfBirth; set => this._dateOfBirth = value; }
+        public string PlaceOfBirth { get => this._placeOfBirth; set => this._placeOfBirth = value; }
+        public string Name { get => this._name; set => this._name = value; }
+        public string Sex { get => this._sex; set => this._sex = value; }
+        public int Age { get => this._age; set => this._age = value; }
+        public int Height { get => this._height; set => this._height = value; }
+        public string Eyes { get => this._eyes; set => this._eyes = value; }
+        public string SkinTone { get => this._skinTone; set => this._skinTone = value; }
+
+    } //Class
+} //Namespace
---
+++ RPG Character Creator/DIceRoll.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_Character_Creator
+{
+    class DiceRoll
+    {
+        // Random number
+        private Random rnd = new Random();
+
+        // Rolling a dice with the previous random number as the maximum
+        public int Roll(int n)
+        {
+            int res;
+            if (n >= 1)
+            {
+                Console.WriteLine("Rolling a d" + n + " dice!");
+                res = this.rnd.Next(1, n);
+            }
+            else
+            { 
+                res = -1;
+            }
+
+            return res;    
+        } //Roll
+    } //Class
+} //Namespace

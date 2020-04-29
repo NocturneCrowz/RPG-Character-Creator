82e14d265f39fed8771d6687c53a3817da88550a
Cristian <531tonk@gmail.com>
2020-04-29 05:22:41 +0200

    Added SearchAFeat
    Added SAF function via SearchAFeat class

--- RPG Character Creator/Program.cs
+++ RPG Character Creator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace RPG_Character_Creator
 {
@@ -6,37 +8,47 @@ namespace RPG_Character_Creator
     {
         static void Main(string[] args)
         {
-            DiceRoll rnd = new DiceRoll();
-            int diceRoll = -1;
-            Console.WriteLine("Choose a number:");
-            do
-            {
-                int value = 0;
-                try
-                {
-                    value = Convert.ToInt32(Console.ReadLine());
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("It seems like you tried to insert a letter instead of an integer. You can't do that!");
-                    Console.WriteLine();
-                }
-                catch (OverflowException)
-                {
-                    Console.WriteLine("It seems like something went wrong. Try contacting the developer, but he won't know much more.");
-                    Console.WriteLine();
-                }
-                finally
-                {
-                    diceRoll = rnd.Roll(value);
-                    if (diceRoll == -1)
-                    {
-                        Console.WriteLine("Whoops! Your dice has fallen into a blackhole, recover it and try again! Choose a number: ");
-                    }
-                }
-            } while (diceRoll == -1);
+            // dice roll
+            /* DiceRoll rnd = new DiceRoll();
+             int diceRoll = -1;
+             Console.WriteLine("Choose a number:");
+             do
+             {
+                 int value = 0;
+                 try
+                 {
+                     value = Convert.ToInt32(Console.ReadLine());
+                 }
+                 catch (FormatException)
+                 {
+                     Console.WriteLine("It seems like you tried to insert one or more letters instead of an integer. You can't do that!");
+                     Console.WriteLine();
+                 }
+                 catch (OverflowException)
+                 {
+                     Console.WriteLine("It seems like something went wrong. Try contacting the developer, but he won't know much more.");
+                     Console.WriteLine();
+                 }
+                 finally
+                 {
+                     diceRoll = rnd.Roll(value);
+                     if (diceRoll == -1)
+                     {
+                         Console.WriteLine("Whoops! Your dice has fallen into a blackhole, recover it and try again! Choose a number: ");
+                     }
+                 }
+             } while (diceRoll == -1);
 
-            Console.WriteLine(diceRoll);
+             Console.WriteLine(diceRoll);*/
+            Console.WriteLine("Path");
+            string path = Console.ReadLine();
+
+            Console.WriteLine("Feat");
+            SearchAFeat search = new SearchAFeat(path);
+            search.SAF(Console.ReadLine());
+           
+            
+            
 
         }
     }
---
+++ RPG Character Creator/SearchAFeat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace RPG_Character_Creator
+{
+    class SearchAFeat
+    {
+        private string path;
+        
+        public void SAF(string keyword)
+        {
+            StreamReader file = new StreamReader(@path);
+            string ln;
+            Console.WriteLine("Searching in the black hole!");
+
+            while ((ln = file.ReadLine()) != null)
+            {
+                if (ln.Contains(keyword + ':', StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Feat found!");
+                    Console.WriteLine(ln);
+                    file.Close();
+                    return;
+                }
+
+            }
+            Console.WriteLine("It seems like I wasn't able to find your feat. Sadly my AI is not that great, blame the developer!");
+            file.Close();
+        }
+
+        public SearchAFeat(string path)
+        {
+            this.path = path;
+        }
+    }
+}

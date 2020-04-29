da4758e85c4222321da10007d56d231759bddda6
Cristian <531tonk@gmail.com>
2020-04-29 05:46:29 +0200

    Added Feats.txt and some comments

--- RPG Character Creator/SearchAFeat.cs
+++ RPG Character Creator/SearchAFeat.cs
@@ -7,16 +7,21 @@ namespace RPG_Character_Creator
 {
     class SearchAFeat
     {
+        // Path to Feats.txt file
         private string path;
         
+        // Search a feat function
         public void SAF(string keyword)
         {
+            // Loading Feats.txt
             StreamReader file = new StreamReader(@path);
             string ln;
-            Console.WriteLine("Searching in the black hole!");
+            Console.WriteLine("Searching into the void...");
 
+            // Reading Feats.txt and comparing the feat needed with the text
             while ((ln = file.ReadLine()) != null)
             {
+                // Succesful search
                 if (ln.Contains(keyword + ':', StringComparison.OrdinalIgnoreCase))
                 {
                     Console.WriteLine("Feat found!");
@@ -26,9 +31,12 @@ namespace RPG_Character_Creator
                 }
 
             }
+            // Insuccesful search
             Console.WriteLine("It seems like I wasn't able to find your feat. Sadly my AI is not that great, blame the developer!");
             file.Close();
         }
+
+        // Public costructor to set the path
 
         public SearchAFeat(string path)
         {
